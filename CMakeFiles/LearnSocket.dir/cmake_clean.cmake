8d5646d8ff8778ed3b947fa3ca8c7fe312e80bc9
Kevinlinpr <kevinlinpr@gmail.com>
2019-01-05 03:45:54 +0800

    first commit

---
+++ CMakeFiles/LearnSocket.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnSocket.dir/main.cpp.o"
+  "LearnSocket.pdb"
+  "LearnSocket"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearnSocket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
